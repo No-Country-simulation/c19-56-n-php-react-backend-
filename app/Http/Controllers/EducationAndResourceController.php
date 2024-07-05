9435075edafe0825dac6667ca17fd80826360dab
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-07-05 13:45:50 -0400

    🎉 : init project

---
+++ app/Http/Controllers/EducationAndResourceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EducationAndResource;
+use Illuminate\Http\Request;
+
+class EducationAndResourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EducationAndResource $educationAndResource)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EducationAndResource $educationAndResource)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EducationAndResource $educationAndResource)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RaceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Race;
+use Illuminate\Http\Request;
+
+class RaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Race $race)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Race $race)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Race $race)
+    {
+        //
+    }
+}
